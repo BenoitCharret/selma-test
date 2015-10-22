c404b80ac06bef4669c1dddcedcbea2c1a0ee8c6
Benoit Charret <bcharret.ext@galerieslafayette.com>
2015-10-22 10:21:42 +0200

    import

--- src/main/java/com/test/MyMapper.java
+++ src/main/java/com/test/MyMapper.java
@@ -1,15 +1,13 @@
 package com.test;
 
 import com.test.from.FromA;
-import com.test.from.SubSubElement;
-import com.test.to.AnotherSubElement;
 import com.test.to.ToA;
 import fr.xebia.extras.selma.Field;
 import fr.xebia.extras.selma.Mapper;
 
 @Mapper(
         withCustomFields = {
-                @Field({"element.subelementlist","infos"})
+                @Field({"element.subelementlist", "infos"})
         },
         withCustom = {SubSubElementMapper.class}
 )
